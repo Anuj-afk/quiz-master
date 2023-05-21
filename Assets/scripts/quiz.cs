3eb8fdc4c4035f022b1d969a3f750980b462f37d
Anuj_Gupta6263 <anujg6263@gmail.com>
2023-05-21 19:07:49 +0530

    added text to single button

---
+++ Assets/scripts/quiz.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questiontext;
+    [SerializeField] QuestionSo question;
+    [SerializeField] GameObject[] answerButtons;
+    void Start()
+    {
+        questiontext.text = question.GetQuestion();
+
+        TextMeshProUGUI buttonText = answerButtons[0].GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = question.GetAnswer(0);
+    }
+}
