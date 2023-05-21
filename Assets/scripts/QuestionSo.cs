497f73cd6efb380ecb00d99aeb1d7e2112ad8405
Anuj_Gupta6263 <anujg6263@gmail.com>
2023-05-21 15:18:52 +0530

    added more getter methods

--- Assets/scripts/QuestionSo.cs
+++ Assets/scripts/QuestionSo.cs
@@ -1,17 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SocialPlatforms;
 
 [CreateAssetMenu(menuName = "quiz question", fileName = "question")]
 public class QuestionSo : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
 
-
+    [SerializeField] [Range(0, 4)] int IndexAns;
     public string GetQuestion()
     {
         return question;
+    }
+    public int GetCorrectAnswer()
+    {
+        return IndexAns;
+    }
+    public string GetAnswer(int index)
+    {
+        return answers[index];
     }
 }
